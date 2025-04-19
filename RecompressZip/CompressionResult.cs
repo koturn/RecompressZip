accc1b49b673a8d6f02f8f7ee64be17721930b18
koturn <jeak.koutan.apple@gmail.com>
2025-04-19 13:33:20 +0900

    refactor: IDE0290, use primary constructor

--- RecompressZip/CompressionResult.cs
+++ RecompressZip/CompressionResult.cs
@@ -6,26 +6,20 @@ namespace RecompressZip
     /// <summary>
     /// Comression result.
     /// </summary>
-    public struct CompressionResult
+    /// <remarks>
+    /// Primary ctor: Initialize all properties.
+    /// </remarks>
+    /// <param name="header">Local file header.</param>
+    /// <param name="offset">Relative offset of local file header.</param>
+    public struct CompressionResult(LocalFileHeader header, uint offset)
     {
         /// <summary>
         /// Local file header.
         /// </summary>
-        public LocalFileHeader Header { get; }
+        public LocalFileHeader Header { get; } = header;
         /// <summary>
         /// Relative offset of local file header.
         /// </summary>
-        public uint Offset { get; set; }
-
-        /// <summary>
-        /// Initialize all properties.
-        /// </summary>
-        /// <param name="header">Local file header.</param>
-        /// <param name="offset">Relative offset of local file header.</param>
-        public CompressionResult(LocalFileHeader header, uint offset)
-        {
-            Header = header;
-            Offset = offset;
-        }
+        public uint Offset { get; set; } = offset;
     }
 }
--- RecompressZip/ExecuteOptions.cs
+++ RecompressZip/ExecuteOptions.cs
@@ -7,7 +7,30 @@ namespace RecompressZip
     /// <summary>
     /// Option values class for execution.
     /// </summary>
-    public class ExecuteOptions : ICloneable
+    /// <remarks>
+    /// Primary ctor: Initialize all members.
+    /// </remarks>
+    /// <param name="numberOfThreads">Number of threads for re-compressing. -1 means unlimited.</param>
+    /// <param name="encodingName">Encoding name of non UTF-8 name and comment of zip entries.</param>
+    /// <param name="password">Password of zip archive.</param>
+    /// <param name="passwordEncodingName">Encoding name of <paramref name="password"/>.</param>
+    /// <param name="isForceCompress">Force compress non compressed data.</param>
+    /// <param name="isRemoveDirectoryEntries">Remove directory entries or not.</param>
+    /// <param name="isVerifyCrc32">Verify CRC-32 value of zip entry or not.</param>
+    /// <param name="isOverwrite">Overwrite original files.</param>
+    /// <param name="isReplaceForce">Do the replacement even if the size of the recompressed data is larger than the size of the original data.</param>
+    /// <param name="isDryRun">Don't save any files, just see the console output.</param>
+    public class ExecuteOptions(
+        int numberOfThreads = ExecuteOptions.DefaultNumberOfThreads,
+        string? encodingName = null,
+        string? password = null,
+        string? passwordEncodingName = null,
+        bool isForceCompress = false,
+        bool isRemoveDirectoryEntries = false,
+        bool isVerifyCrc32 = false,
+        bool isOverwrite = false,
+        bool isReplaceForce = false,
+        bool isDryRun = false) : ICloneable
     {
         /// <summary>
         /// Default value for <see cref="NumberOfThreads"/>.
@@ -17,86 +40,49 @@ namespace RecompressZip
         /// <summary>
         /// Number of threads for re-compressing. -1 means unlimited.
         /// </summary>
-        public int NumberOfThreads { get; set; }
+        public int NumberOfThreads { get; set; } = numberOfThreads;
         /// <summary>
         /// Encoding name of non UTF-8 name and comment of zip entries.
         /// </summary>
-        public string? EncodingName { get; set; }
+        public string? EncodingName { get; set; } = encodingName;
         /// <summary>
         /// Password of zip archive.
         /// </summary>
-        public string? Password { get; set; }
+        public string? Password { get; set; } = password;
         /// <summary>
         /// Encoding name of <see cref="Password"/>.
         /// </summary>
-        public string? PasswordEncodingName { get; set; }
+        public string? PasswordEncodingName { get; set; } = passwordEncodingName;
         /// <summary>
         /// Force compress non compressed data.
         /// </summary>
-        public bool IsForceCompress { get; set; }
+        public bool IsForceCompress { get; set; } = isForceCompress;
         /// <summary>
         /// Remove directory entries or not.
         /// </summary>
-        public bool IsRemoveDirectoryEntries { get; set; }
+        public bool IsRemoveDirectoryEntries { get; set; } = isRemoveDirectoryEntries;
         /// <summary>
         /// Verify CRC-32 value of zip entry or not.
         /// </summary>
-        public bool IsVerifyCrc32 { get; set; }
+        public bool IsVerifyCrc32 { get; set; } = isVerifyCrc32;
         /// <summary>
         /// Overwrite original files.
         /// </summary>
-        public bool IsOverwrite { get; set; }
+        public bool IsOverwrite { get; set; } = isOverwrite;
         /// <summary>
         /// Do the replacement even if the size of the recompressed data is larger than the size of the original data.
         /// </summary>
-        public bool IsReplaceForce { get; set; }
+        public bool IsReplaceForce { get; set; } = isReplaceForce;
         /// <summary>
         /// Don't save any files, just see the console output.
         /// </summary>
-        public bool IsDryRun { get; set; }
+        public bool IsDryRun { get; set; } = isDryRun;
         /// <summary>
         /// True if <see cref="IsOverwrite"/> is false and <see cref="IsDryRun"/> is false.
         /// </summary>
         public bool IsCreateNewFile
         {
             get { return !IsOverwrite && !IsDryRun; }
-        }
-
-        /// <summary>
-        /// Initialize all members.
-        /// </summary>
-        /// <param name="numberOfThreads">Number of threads for re-compressing. -1 means unlimited.</param>
-        /// <param name="encodingName">Encoding name of non UTF-8 name and comment of zip entries.</param>
-        /// <param name="password">Password of zip archive.</param>
-        /// <param name="passwordEncodingName">Encoding name of <paramref name="password"/>.</param>
-        /// <param name="isForceCompress">Force compress non compressed data.</param>
-        /// <param name="isRemoveDirectoryEntries">Remove directory entries or not.</param>
-        /// <param name="isVerifyCrc32">Verify CRC-32 value of zip entry or not.</param>
-        /// <param name="isOverwrite">Overwrite original files.</param>
-        /// <param name="isReplaceForce">Do the replacement even if the size of the recompressed data is larger than the size of the original data.</param>
-        /// <param name="isDryRun">Don't save any files, just see the console output.</param>
-        public ExecuteOptions(
-            int numberOfThreads = DefaultNumberOfThreads,
-            string? encodingName = null,
-            string? password = null,
-            string? passwordEncodingName = null,
-            bool isForceCompress = false,
-            bool isRemoveDirectoryEntries = false,
-            bool isVerifyCrc32 = false,
-            bool isOverwrite = false,
-            bool isReplaceForce = false,
-            bool isDryRun = false)
-        {
-            NumberOfThreads = numberOfThreads;
-            EncodingName = encodingName;
-            Password = password;
-            PasswordEncodingName = passwordEncodingName;
-            IsForceCompress = isForceCompress;
-            IsRemoveDirectoryEntries = isRemoveDirectoryEntries;
-            IsVerifyCrc32 = isVerifyCrc32;
-            IsOverwrite = isOverwrite;
-            IsReplaceForce = isReplaceForce;
-            IsDryRun = isDryRun;
         }
 
         /// <summary>
