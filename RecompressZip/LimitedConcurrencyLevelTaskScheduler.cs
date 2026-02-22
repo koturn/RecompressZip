5ad55ab4a45e9360914fb73bac896eb2cdb0d0f7
koturn <jeak.koutan.apple@gmail.com>
2026-02-22 16:00:06 +0900

    refactor: Upgrade LimitedConcurrencyLevelTaskScheduler

--- RecompressZip/LimitedConcurrencyLevelTaskScheduler.cs
+++ RecompressZip/LimitedConcurrencyLevelTaskScheduler.cs
@@ -2,19 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-#if !NET8_0_OR_GREATER
-using RecompressZip.Internals;
-#endif
 
 
 namespace RecompressZip
 {
     /// <summary>
-    /// Provides a task scheduler that ensures a maximum concurrency level while
-    /// running on top of the thread pool.
-    /// Implementation of this class if found on https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.taskscheduler?view=netframework-4.7.2
+    /// Provides a task scheduler that ensures a maximum concurrency level while running on top of the thread pool.
     /// </summary>
-    public class LimitedConcurrencyLevelTaskScheduler : TaskScheduler
+    /// <remarks>
+    /// <seealso href="https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.taskscheduler"/>
+    /// </remarks>
+    public sealed class LimitedConcurrencyLevelTaskScheduler : TaskScheduler
     {
         /// <summary>
         /// Indicates whether the current thread is processing work items.
@@ -22,36 +20,88 @@ namespace RecompressZip
         [ThreadStatic]
         private static bool _currentThreadIsProcessingItems;
 
-
         /// <summary>
         /// The maximum concurrency level allowed by this scheduler.
         /// </summary>
-        public sealed override int MaximumConcurrencyLevel { get; }
-
+        public sealed override int MaximumConcurrencyLevel => _maxDegreeOfParallelism;
 
         /// <summary>
-        /// The list of tasks to be executed
+        /// The list of tasks to be executed.
         /// </summary>
-        private readonly LinkedList<Task> _tasks = new(); // protected by lock(_tasks)
+        private readonly LinkedList<Task> _tasks = new();
         /// <summary>
-        /// Indicates whether the scheduler is currently processing work items.
+        /// The maximum concurrency level allowed by this scheduler.
+        /// </summary>
+        private int _maxDegreeOfParallelism;
+        /// <summary>
+        /// <para>Indicates whether the scheduler is currently processing work items.</para>
+        /// <para>This variable locked by <see cref="_taskListLock"/>.</para>
         /// </summary>
         private int _delegatesQueuedOrRunning = 0;
+        /// <summary>
+        /// Lock object for <see cref="_tasks"/> and <see cref="_delegatesQueuedOrRunning"/>.
+        /// </summary>
+#if NET9_0_OR_GREATER
+        private readonly Lock _taskListLock = new();
+#else
+        private readonly object _taskListLock = new();
+#endif  // !NET9_0_OR_GREATER
+        /// <summary>
+        /// True to allow non long running task in new (non pooled) thread.
+        /// </summary>
+        private readonly bool _allowTORunNonLongRunningTaskOnNewThread;
 
 
         /// <summary>
         /// Creates a new instance with the specified degree of parallelism.
         /// </summary>
         /// <param name="maxDegreeOfParallelism"></param>
-        public LimitedConcurrencyLevelTaskScheduler(int maxDegreeOfParallelism)
+        /// <param name="allowRunNonLongRunningTaskInNonPooledThread">True to allow non long running task in new (non pooled) thread.</param>
+        public LimitedConcurrencyLevelTaskScheduler(int maxDegreeOfParallelism, bool allowRunNonLongRunningTaskInNonPooledThread = true)
         {
 #if NET8_0_OR_GREATER
             ArgumentOutOfRangeException.ThrowIfLessThan(maxDegreeOfParallelism, 1);
 #else
-            ThrowHelper.ThrowIfLessThan(maxDegreeOfParallelism, 1);
+            ThrowIfLessThan(maxDegreeOfParallelism, 1, nameof(maxDegreeOfParallelism));
 #endif  // NET8_0_OR_GREATER
-            MaximumConcurrencyLevel = maxDegreeOfParallelism;
+            _maxDegreeOfParallelism = maxDegreeOfParallelism;
+            _allowTORunNonLongRunningTaskOnNewThread = allowRunNonLongRunningTaskInNonPooledThread;
         }
+
+
+        /// <summary>
+        /// Changes <see cref="MaximumConcurrencyLevel"/>.
+        /// </summary>
+        /// <param name="maxDegreeOfParallelism"></param>
+        public void SetMaximumConcurrencyLevel(int maxDegreeOfParallelism)
+        {
+#if NET8_0_OR_GREATER
+            ArgumentOutOfRangeException.ThrowIfLessThan(maxDegreeOfParallelism, 1);
+#else
+            ThrowIfLessThan(maxDegreeOfParallelism, 1, nameof(maxDegreeOfParallelism));
+#endif  // NET8_0_OR_GREATER
+            var maxDegreeOfParallelismOld = _maxDegreeOfParallelism;
+            _maxDegreeOfParallelism = maxDegreeOfParallelism;
+
+            if (maxDegreeOfParallelismOld >= maxDegreeOfParallelism)
+            {
+                return;
+            }
+
+            lock (_taskListLock)
+            {
+                var diff = Math.Min(maxDegreeOfParallelism, _tasks.Count) - _delegatesQueuedOrRunning;
+                if (diff > 0)
+                {
+                    _delegatesQueuedOrRunning += diff;
+                    for (int i = 0; i < diff; i++)
+                    {
+                        NotifyThreadPoolOfPendingWork();
+                    }
+                }
+            }
+        }
+
 
         /// <summary>
         /// Queues a task to the scheduler.
@@ -59,21 +109,107 @@ namespace RecompressZip
         /// <param name="task">A task.</param>
         protected sealed override void QueueTask(Task task)
         {
-            // Add the task to the list of tasks to be processed. If there aren't enough
-            // delegates currently queued or running to process tasks, schedule another.
-            lock (_tasks)
+            lock (_taskListLock)
             {
-                _tasks.AddLast(task);
-                if (_delegatesQueuedOrRunning < MaximumConcurrencyLevel)
+                if (_delegatesQueuedOrRunning < _maxDegreeOfParallelism)
                 {
                     _delegatesQueuedOrRunning++;
-                    NotifyThreadPoolOfPendingWork();
+                    if ((task.CreationOptions & TaskCreationOptions.LongRunning) == TaskCreationOptions.None)
+                    {
+                        // Add the task to the list of tasks to be processed.
+                        _tasks.AddLast(task);
+                        NotifyThreadPoolOfPendingWork();
+                    }
+                    else
+                    {
+                        // Run the long running task on the new thread.
+                        RunTaskOnNewThread(task);
+                    }
+                }
+                else
+                {
+                    // If there aren't enough delegates currently queued or running to process tasks, schedule another.
+                    _tasks.AddLast(task);
                 }
             }
         }
 
         /// <summary>
-        /// Inform the ThreadPool that there's work to be executed for this scheduler.
+        /// Attempts to execute the specified task on the current thread.
+        /// </summary>
+        /// <param name="task">A task to execute.</param>
+        /// <param name="taskWasPreviouslyQueued">A flag whether <paramref name="task"/> is queued previously.</param>
+        /// <returns><c>true</c> if task was successfully executed, <c>false</c> if it was not.</returns>
+        protected sealed override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            // If this thread isn't already processing a task, we don't support inlining.
+            // If the task was previously queued, remove it from the queue.
+            return _currentThreadIsProcessingItems
+                && (!taskWasPreviouslyQueued || TryDequeue(task))
+                && TryExecuteTask(task);
+        }
+
+        /// <summary>
+        /// Attempt to remove a previously scheduled task from the scheduler.
+        /// </summary>
+        /// <param name="task">A task to remove from the scheduler</param>
+        /// <returns><c>true</c> if the element containing value is successfully removed; otherwise, <c>false</c>.</returns>
+        protected sealed override bool TryDequeue(Task task)
+        {
+            lock (_taskListLock)
+            {
+                return _tasks.Remove(task);
+            }
+        }
+
+        /// <summary>
+        /// Gets an enumerable of the tasks currently scheduled on this scheduler.
+        /// </summary>
+        /// <returns>An enumerator of tasks.</returns>
+        protected sealed override IEnumerable<Task> GetScheduledTasks()
+        {
+            var lockTaken = false;
+
+#if NET9_0_OR_GREATER
+            try
+            {
+                lockTaken = _taskListLock.TryEnter();
+                if (!lockTaken)
+                {
+                    throw new NotSupportedException();
+                }
+                return _tasks;
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    _taskListLock.Exit();
+                }
+            }
+#else
+            try
+            {
+                Monitor.TryEnter(_taskListLock, ref lockTaken);
+                if (!lockTaken)
+                {
+                    throw new NotSupportedException();
+                }
+                return _tasks;
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Monitor.Exit(_taskListLock);
+                }
+            }
+#endif  // NET9_0_OR_GREATER
+        }
+
+
+        /// <summary>
+        /// Inform the <see cref="ThreadPool"/> that there's work to be executed for this scheduler.
         /// </summary>
         private void NotifyThreadPoolOfPendingWork()
         {
@@ -87,26 +223,41 @@ namespace RecompressZip
                     // Process all available items in the queue.
                     while (true)
                     {
-                        Task item;
-                        lock (_tasks)
+                        Task task;
+                        lock (_taskListLock)
                         {
-                            // When there are no more items to be processed,
-                            // note that we're done processing, and get out.
-                            var task = _tasks.First;
-                            if (task is null)
+                            // Terminate tasks exceeding the maximum concurrency level.
+                            if (_delegatesQueuedOrRunning > _maxDegreeOfParallelism)
                             {
                                 _delegatesQueuedOrRunning--;
                                 break;
                             }
-                            // Get the next item from the queue
-                            item = task.Value;
+                            // When there are no more items to be processed,
+                            // note that we're done processing, and get out.
+                            var node = _tasks.First;
+                            if (node == null)
+                            {
+                                _delegatesQueuedOrRunning--;
+                                break;
+                            }
+                            // Get the next item from the queue.
+                            task = node.Value;
                             _tasks.RemoveFirst();
                         }
-                        // Execute the task we pulled out of the queue
-                        TryExecuteTask(item);
+
+                        // If the task we pulled out is a long running task,
+                        // executing the task on the pool thread.
+                        if ((task.CreationOptions & TaskCreationOptions.LongRunning) != TaskCreationOptions.None)
+                        {
+                            RunTaskOnNewThread(task);
+                            break;
+                        }
+
+                        // Execute the task we pulled out of the queue.
+                        TryExecuteTask(task);
                     }
                 }
-                // We're done processing items on the current thread
+                // We're done processing items on the current thread.
                 finally
                 {
                     _currentThreadIsProcessingItems = false;
@@ -115,56 +266,101 @@ namespace RecompressZip
         }
 
         /// <summary>
-        /// Attempts to execute the specified task on the current thread.
+        /// Run specified task on new (non pooled) thread.
         /// </summary>
         /// <param name="task">A task to execute.</param>
-        /// <param name="taskWasPreviouslyQueued">A flag whether <paramref name="task"/> is queued previously.</param>
-        /// <returns><c>true</c> if task was successfully executed, <c>false</c> if it was not.</returns>
-        protected sealed override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        private void RunTaskOnNewThread(Task task)
         {
-            // If this thread isn't already processing a task, we don't support inlining
-            // If the task was previously queued, remove it from the queue
-            return _currentThreadIsProcessingItems
-                && (!taskWasPreviouslyQueued || TryDequeue(task))
-                && TryExecuteTask(task);
+            new Thread(param =>
+            {
+                // Note that the current thread is now processing work items.
+                // This is necessary to enable inlining of tasks into this thread.
+                _currentThreadIsProcessingItems = true;
+                try
+                {
+                    TryExecuteTask((Task)param!);
+                    while (true)
+                    {
+                        Task nextTask;
+                        lock (_taskListLock)
+                        {
+                            if (_delegatesQueuedOrRunning > _maxDegreeOfParallelism)
+                            {
+                                _delegatesQueuedOrRunning--;
+                                break;
+                            }
+                            // When there are no more items to be processed,
+                            // note that we're done processing, and get out.
+                            var node = _tasks.First;
+                            if (node == null)
+                            {
+                                _delegatesQueuedOrRunning--;
+                                break;
+                            }
+                            // Get the next item from the queue
+                            nextTask = node.Value;
+
+                            // If the task we pulled out is not a long running task and not allowed in non pooled thread,
+                            // executing the task on the pool thread.
+                            if (!_allowTORunNonLongRunningTaskOnNewThread
+                                && (nextTask.CreationOptions & TaskCreationOptions.LongRunning) == TaskCreationOptions.None)
+                            {
+                                NotifyThreadPoolOfPendingWork();
+                                break;
+                            }
+
+                            _tasks.RemoveFirst();
+                        }
+
+                        // Execute the task we pulled out of the queue.
+                        TryExecuteTask(nextTask);
+                    }
+                }
+                // We're done processing items on the current thread.
+                finally
+                {
+                    _currentThreadIsProcessingItems = false;
+                }
+            })
+            {
+                IsBackground = true
+            }.Start(task);
+        }
+
+
+#if !NET8_0_OR_GREATER
+        /// <summary>
+        /// Throw <see cref="ArgumentOutOfRangeException"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the objects.</typeparam>
+        /// <param name="value">The maxDegreeOfParallelism of the argument that causes this exception.</param>
+        /// <param name="other">The maxDegreeOfParallelism to compare with <paramref name="value"/>.</param>
+        /// <param name="paramName">The name of the parameter with which <paramref name="value"/> corresponds.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Always thrown.</exception>
+#if NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1
+        [DoesNotReturn]
+#endif  // NETCOREAPP3_0_OR_GREATER || NETSTANDARD2_1
+        private static void ThrowLess<T>(T value, T other, string paramName)
+        {
+            throw new ArgumentOutOfRangeException(paramName, value, $"'{value}' must be greater than or equal to '{other}'.");
         }
 
         /// <summary>
-        /// Attempt to remove a previously scheduled task from the scheduler.
+        /// Throws an <see cref="ArgumentOutOfRangeException"/> if <paramref name="value"/> is less than <paramref name="other"/>.
         /// </summary>
-        /// <param name="task">A task to remove from the scheduler</param>
-        /// <returns><c>true</c> if the element containing value is successfully removed; otherwise, <c>false</c>.</returns>
-        protected sealed override bool TryDequeue(Task task)
+        /// <typeparam name="T">The type of the objects to validate.</typeparam>
+        /// <param name="value">The argument to validate as greater than or equal to <paramref name="other"/>.</param>
+        /// <param name="other">The maxDegreeOfParallelism to compare with <paramref name="value"/>.</param>
+        /// <param name="paramName">The name of the parameter with which <paramref name="value"/> corresponds.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="value"/> is less than <paramref name="other"/>.</exception>
+        internal static void ThrowIfLessThan<T>(T value, T other, string paramName)
+            where T : IComparable<T>
         {
-            lock (_tasks)
+            if (value.CompareTo(other) < 0)
             {
-                return _tasks.Remove(task);
+                ThrowLess(value, other, paramName);
             }
         }
-
-        /// <summary>
-        /// Gets an enumerable of the tasks currently scheduled on this scheduler.
-        /// </summary>
-        /// <returns>An enumerator of tasks.</returns>
-        protected sealed override IEnumerable<Task> GetScheduledTasks()
-        {
-            var lockTaken = false;
-            try
-            {
-                Monitor.TryEnter(_tasks, ref lockTaken);
-                if (!lockTaken)
-                {
-                    throw new NotSupportedException();
-                }
-                return _tasks;
-            }
-            finally
-            {
-                if (lockTaken)
-                {
-                    Monitor.Exit(_tasks);
-                }
-            }
-        }
+#endif  // !NET8_0_OR_GREATER
     }
 }
